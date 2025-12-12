0d82a4a9c810c1b1e33c558384069e649154ff06
Adrian Feijó Fagundes <adrianfeijofagundes@gmail.com>
2025-12-11 21:26:52 -0300

    delete loginreserva.txt

--- services/user.ts
+++ services/user.ts
@@ -1,7 +1,6 @@
 import { executeWithErrorHandling } from "@/utils/executeWithErrorHandling";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import api from "./api";
-import { storage } from "./storage";
 
 export async function getUserLocal() {
     return executeWithErrorHandling(async () => {
